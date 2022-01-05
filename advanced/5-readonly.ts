744b5c0eed5bb832449769ed5eb2316c1f558149
ne-eun <p.tyche92@gmail.com>
2022-01-05 15:43:19 +0900

    #readonly

--- advanced/5-readonly.ts
+++ advanced/5-readonly.ts
@@ -1,9 +1,11 @@
 {
   type ToDo = {
-    title: string;
-    description: string;
-  };
+    title: string
+    description: string
+  }
 
+  // Readonly는 타입스크립트에만 있음
+  // javascript는 없음
   function display(todo: Readonly<ToDo>) {
     // todo.title = 'jaja';
   }
